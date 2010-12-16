b8dfd21948f96367299abfdc9626b05e953017dc
Ilya Rogozin <ilya.rogozin@gmail.com>
2010-12-16 23:44:37 +0300

    fixed #30 fixed #34

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
 def show
     @category = Category.find_by_permalink(params[:id])
+    @ltp = url_for(:only_path => false, :controller => controller_name, :action => action_name, :id => @category.permalink, :page=>params[:page], :per_page=>params[:per_page])    
     params[:page] ||=1
     @products = Product.find_all({:page=>params[:page], :per_page=>params[:per_page], :category=> @category.id})
     #render :template =>'categories/show', :layout => false if request.xhr?    
