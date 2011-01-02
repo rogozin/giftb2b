014429c3bc11fc6c23c80784d6306d3ab033eaba
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-01-02 17:50:33 +0300

    fixed #226 time 0.5 hours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110102110959) do
+ActiveRecord::Schema.define(:version => 20110102141842) do
 
   create_table "attach_images", :id => false, :force => true do |t|
     t.integer "attachable_id"
