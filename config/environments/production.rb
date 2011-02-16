0b2cf48cd9e98135d90c5e3ca4a130ae05600474
root <root@s01.giftb2b.ru>
2011-02-16 22:57:48 +0300

    fixed i18n production locale

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Giftr3::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
+  config.i18n.locale = :ru
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
