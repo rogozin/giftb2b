3b33ac2da92ec083d595e81eb188296d5814ea08
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-03-21 21:28:04 +0300

    fixed #356 @1h

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,7 +8,7 @@ class MainController < ApplicationController
   end
   
   def search
-    @products = (current_user && (current_user.is_firm_user? || current_user.is_admin_user?) ? Product.search_with_article(params[:request]) : Product.search(params[:request])).paginate(:page => params[:page])
+    @products = (current_user && (current_user.is_firm_user? || current_user.is_admin_user?) ? Product.search_with_article(params[:request]) : Product.search(params[:request])).paginate(:page => params[:page], :per_page => params[:per_page] || "20")
   end
 
 
