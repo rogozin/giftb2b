bb38e8d6f27048fab140ec4a668fbd1e01db028b
root <root@s01.giftb2b.ru>
2011-03-31 00:00:49 +0400

    fix pdf params

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,9 +1,11 @@
 PDFKit.configure do |config|
   config.default_options = {
-   :margin_top => '1cm',
-   :margin_right => '1cm',
-   :margin_bottom => '1cm',
-   :margin_left => '1cm',
+   :margin_top => '10mm',
+   :margin_right => '10mm',
+   :margin_bottom => '10mm',
+   :margin_left => '10mm',
+   :image_quality => 100,
+   :image_dpi => 1200
   }
 end
 
