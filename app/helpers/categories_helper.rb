2536798b23e92e9e84e0ea131ce1e2929beb37ae
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-04-24 00:33:51 +0400

    refs #546 add p tag

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -8,7 +8,7 @@ module CategoriesHelper
     res << "<ul class='ul_r'>"
     parent_category.children.each_with_index do |category, ind|
        res << "<li class='li_r'>" if (ind % 7).zero?
-       res << link_to(category.name, category_path(category), :class => "menu_left")
+       res << content_tag(:p,link_to(category.name, category_path(category), :class => "menu_left"))
        res << " "
        res << "</li>"if ((ind+1) % 7).zero? || parent_category.children.size == ind + 1
     end
