8853e68d431c7c2f1fadb17ad641032a179acaaf
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-04-23 23:36:17 +0400

    fixed #546 @1.5h  add catalog_children helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,4 +3,17 @@ module CategoriesHelper
     session[:category_location] && session[:category_location] == current_permalink ?  "selected" : nil 
   end
   
+  def catalog_children parent_category
+    res = ""
+    res << "<ul class='ul_r'>"
+    parent_category.children.each_with_index do |category, ind|
+       res << "<li class='li_r'>" if (ind % 7).zero?
+       res << link_to(category.name, category_path(category), :class => "menu_left")
+       res << " "
+       res << "</li>"if ((ind+1) % 7).zero? || parent_category.children.size == ind + 1
+    end
+    res << "</ul>"
+    raw res
+  end
+  
 end
