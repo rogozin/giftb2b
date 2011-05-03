2655ae57ec0444031a4be21b2554d3658dfd5ede
Ilya Rogozin <ilya@rogozun@gmail.com>
2011-05-04 00:32:32 +0400

    fixed #552 @1.3h

--- spec/requests/virtual_category_spec.rb
+++ spec/requests/virtual_category_spec.rb
@@ -1,10 +1,10 @@
-requirerequire 'spec_helper'
+require 'spec_helper'
 
 describe "Работа с вирутальной категорией" do 
   context "Поведение виртуальной категории на главной странице" do 
     before(:each) do
       @description = "Это виртуальная категория. Она может объединять в себе неограниченное количество других категорий каталога"
-      @virtual_category =  Factory(:category, :kind => 0, :description => @description )
+      @virtual_category =  Factory(:category, :kind => 0, :show_description => true, :description => @description )
       @cat1 = Factory(:category, :virtual_id => @virtual_category.id)
     end
     
@@ -12,21 +12,29 @@ describe "Работа с вирутальной категорией" do
       before(:each) do
         visit "/"
         within "#virtual_catalog" do
-          click_link @virtual_catalog.name      
+          click_link @virtual_category.name      
         end        
       end
 
       it 'я должен увидеть описание' do
         within "#main_content" do
-          page.should have_content @virtual_catalog.name
+          page.should have_content @virtual_category.name
           page.should have_content @description
         end        
+      end
+
+      it 'я должен увидеть картинку' do
+        img = Image.new :picture => File.new(File.join(Rails.root, '/public/images/default_image.jpg'))
+        @virtual_category.images<< img
+        visit category_path(@virtual_category)
+        page.should have_selector "#virtual_image img"
+        save_and_open_page
       end
       
       it 'Если описание запрещено к показу, то я его не увижу' do
         @virtual_category.toggle!(:show_description)
-        visit categories_path(@virtual_category)
-        page.should have_not_content(@description)
+        visit category_path(@virtual_category)
+        page.should have_no_content(@description)
       end
       
     end #context
