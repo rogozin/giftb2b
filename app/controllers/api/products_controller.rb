8a975a13d9a941fd5dbbecf4e97d862b8df29117
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-06-06 13:55:31 +0400

    finish paginations for api @1h

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,8 +2,10 @@ class Api::ProductsController < Api::BaseController
   
   def index
     params[:page] ||=1
+    params[:per_page] ||=30
+    params[:per_page] = 100 if params[:per_page].to_i > 100
     @products = if params[:category].to_i > 0
-      Product.find_all({:category=> params[:category] }, "json").paginate(:page => params[:page])
+      Product.find_all({:category=> params[:category] }, "json").paginate(:page => params[:page], :per_page => params[:per_page])
     else
       Product.active
     end
