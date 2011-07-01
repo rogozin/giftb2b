5335723e57d8f03c0c378e0861a83a90d1d46c34
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-07-01 13:35:02 +0400

    send_data when xml exporting

--- app/controllers/admin/export_data_controller.rb
+++ app/controllers/admin/export_data_controller.rb
@@ -7,7 +7,8 @@ class Admin::ExportDataController < Admin::BaseController
     
   def export
     @products  = Product.find_all(params,"xml") 
-    render :xml => XmlDownload.get_xml(@products,params[:options])
+    sup_name = Supplier.find(params[:supplier]).name if params[:supplier]      
+    send_data(XmlDownload.get_xml(@products,params[:options]), :type => :xml, :filename => "export_#{sup_name}_#{Date.today}.xml")
   end
     
   def permalinks
