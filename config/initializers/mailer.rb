033d988129def55952e972dec13a4a1c45de5cb0
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-07-15 07:54:04 +0400

    change sendmail to smtp

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,20 +1,21 @@
 Giftr3::Application.configure do
-config.action_mailer.delivery_method = :sendmail
+#config.action_mailer.delivery_method = :sendmail
 # Defaults to:
 # config.action_mailer.sendmail_settings = {
 #   :location => '/usr/sbin/sendmail',
 #   :arguments => '-i -t'
 # }
 
-#config.action_mailer.delivery_method = :smtp
-#config.action_mailer.smtp_settings = {
-#  :address              => "smtp.gmail.com",
-#  :port                 => 587,
-#  :domain               => 'baci.lindsaar.net',
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "127.0.0.1",
+  :port                 => 25,
+  :domain               => 's01.giftb2b.ru',
 #  :user_name            => '***',
 #  :password             => '***',
-#  :authentication       => 'plain',
-#  :enable_starttls_auto => true  }
+  :authentication       => "none",
+#  :enable_starttls_auto => true  
+}
 
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
