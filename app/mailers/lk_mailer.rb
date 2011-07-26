7cf138da107e1983f52ed2683082196b796fd35e
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-07-26 11:21:37 +0400

    modify rufus AR connection:

--- app/mailers/lk_mailer.rb
+++ app/mailers/lk_mailer.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8;
 class LkMailer < ActionMailer::Base
-   default :from => "notification@giftb2b.ru"
+   default :from => "notification@giftb2b.ru", :charset => "UTF-8"
    layout "/layouts/mailer"
 
   def returning_sample_to_supplier(sample)
--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-scheduler.cron "* * * * *" do
-  SampleNotificationJob.new.perform
+scheduler.every "10s", :blocking => true do
+  safely do
+    SampleNotificationJob.new.perform
+  end
 end
+
+ def safely
+      begin
+        unless ActiveRecord::Base.connected?
+          ActiveRecord::Base.connection.verify!(0)
+        end
+        yield
+      rescue => e
+        #status e.inspect
+        puts "err: #{e.message}"
+      ensure
+        ActiveRecord::Base.connection_pool.release_connection
+      end
+    end
