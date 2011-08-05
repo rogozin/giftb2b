cda51791be6429ad0ec3847916ef24e121c84da9
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-08-05 14:20:15 +0400

    fixed script

--- db/migrate/20110805042728_move_colors_and_facturs_to_props.rb
+++ db/migrate/20110805042728_move_colors_and_facturs_to_props.rb
@@ -3,7 +3,7 @@ class MoveColorsAndFactursToProps < ActiveRecord::Migration
   def self.up
     materials = Property.create(:name => "Материал", :sort_order => 0, :for_all_products => true, :for_search => true, :type => 0)
     colors = Property.create(:name => "Цвет", :sort_order => 0, :for_all_products => true, :for_search => true, :type => 0)
-    Product.where("factur is not null").map(&:factur).uniq.compact.map{|x|x.split('/').map{|xx| xx.mb_chars.capitalize.to_s}}.flatten.uniq.each do |factur|
+    Product.where("factur is not null").map(&:factur).uniq.compact.map{|x|x.split(/[,\/]/).map{|xx| xx.strip.mb_chars.capitalize.to_s}}.flatten.uniq.each do |factur|
       materials.property_values.create(:value => factur)
     end
     Product.where("color is not null").map(&:color).uniq.each do |color|
@@ -13,7 +13,7 @@ class MoveColorsAndFactursToProps < ActiveRecord::Migration
     
     Product.all.each do |product|
       if product.factur.present?
-        product.factur.split('/').map{|xx| xx.mb_chars.capitalize.to_s}.each do |factur|
+        product.factur.split(/[,\/]/).map{|xx| xx.strip.mb_chars.capitalize.to_s}.each do |factur|
           mat = materials.property_values.where(:value => factur).first
           product.product_properties.create(:property_value => mat) if mat
         end
