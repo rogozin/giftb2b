dfee63abed36e18683521bf68d1371e78c9f65f8
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-08-10 19:10:08 +0400

    возможность просматривать список товаров у property_value

--- app/controllers/admin/property_values_controller.rb
+++ app/controllers/admin/property_values_controller.rb
@@ -3,29 +3,28 @@ class Admin::PropertyValuesController < Admin::BaseController
   access_control do
      allow :Администратор, "Редактор каталога"
   end
+
+  before_filter :find_pv, :only => [:edit, :update, :destroy, :show]
   
-def new
-  @property = Property.find(params[:property_id])
-  @property_value = PropertyValue.new(:property_id=> @property.id)
-end
+  def new
+    @property = Property.find(params[:property_id])
+    @property_value = PropertyValue.new(:property_id=> @property.id)
+  end
 
-def create
-  @property_value = PropertyValue.new params[:property_value]
-  if @property_value.save
-    flash[:notice] = "Значение создано!"
-    redirect_to admin_properties_path
-  else 
-    render 'new'
-  end  
-end
+  def create
+    @property_value = PropertyValue.new params[:property_value]
+    if @property_value.save
+      flash[:notice] = "Значение создано!"
+      redirect_to admin_properties_path
+    else 
+      render 'new'
+    end  
+  end
 
-def edit
-  @property_value = PropertyValue.find(params[:id])
-  
-end
+  def edit
+  end
 
-def update
- @property_value = PropertyValue.find(params[:id])
+  def update
   if @property_value.update_attributes params[:property_value]
     flash[:notice] = "Значение изменено!"
     redirect_to admin_properties_path
@@ -34,10 +33,20 @@ def update
   end  
 end
 
-def destroy
-  p=PropertyValue.find(params[:id])
- flash[:notice] = "Значение удалено!" if  p.destroy
-  redirect_to admin_properties_path
-end
+  def destroy
+    flash[:notice] = "Значение удалено!" if  @property_value.destroy
+    redirect_to admin_properties_path
+  end
+
+  def show
+    params[:page] ||=1     
+    @products = @property_value.products.paginate(:page => params[:page])
+  end
+
+  private
+  
+  def find_pv
+     @property_value = PropertyValue.find(params[:id])
+  end
 
 end
