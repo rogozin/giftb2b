c9da4ecb0537d55f6218b9ab05aaab682f583fd0
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-08-15 14:54:28 +0400

    fixed #1364

--- lib/excel.rb
+++ lib/excel.rb
@@ -7,6 +7,10 @@ module Gift
          workbook = WriteExcel.new(xls_stream)
          worksheet  = workbook.add_worksheet()
          header  = workbook.add_format(:bold => 1, :size => 18)
+         format1 = workbook.add_format()
+         format1.set_text_wrap()
+         format1.set_align('vcenter')
+         
          worksheet.insert_image(0,0, commercial_offer.firm.logo.path(:thumb)) if commercial_offer.firm.logo
          worksheet.write(0, 2, commercial_offer.firm.short_name,header )  
          worksheet.set_row(0,85)
@@ -17,7 +21,7 @@ module Gift
          commercial_offer.commercial_offer_items.each_with_index do |i, index|
            worksheet.set_row(8+index, 100)
            worksheet.insert_image(8 + index, 0, i.lk_product.picture.path)
-           worksheet.write_row(8 + index, 2, [i.lk_product.article, i.lk_product.short_name, i.lk_product.price, i.quantity, i.quantity * i.lk_product.price, i.lk_product.factur, i.lk_product.color, i.lk_product.size, i.lk_product.box, i.lk_product.infliction, i.lk_product.store_count, i.lk_product.description])
+           worksheet.write_row(8 + index, 2, [i.lk_product.article, i.lk_product.short_name, i.lk_product.price, i.quantity, i.quantity * i.lk_product.price, i.lk_product.factur, i.lk_product.color, i.lk_product.size, i.lk_product.box, i.lk_product.infliction, i.lk_product.store_count, i.lk_product.description],format1)
          end
          workbook.close
          xls_stream.string
