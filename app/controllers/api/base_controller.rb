533d72a75eb2ca85efefa03a3ea013ffef928134
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-08-19 16:14:36 +0400

    fixed ext-search js

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -13,9 +13,11 @@ class Api::BaseController < ActionController::Base
   
   private
   def authorization
-   #request.headers.each {|k,v| puts "#{k} = #{v}"}
+   request.headers.each {|k,v| logger.info "#{k} = #{v}"}
+   logger.info "==request auth=#{request.authorization.to_s}"
    authenticate_or_request_with_http_token do |t,o|
       access = ForeignAccess.accepted_clients.select{|x| x.param_key == t}
+      logger.info "token=#{t} opts=#{o}"
       @firm = access.first.firm if access.present?
       access.present?
     end
