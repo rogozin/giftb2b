3458f8fed47362126e8518118103be1880c871ee
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-09-01 22:50:51 +0400

    fix xml export troubles when supplier is blank

--- app/controllers/admin/export_data_controller.rb
+++ app/controllers/admin/export_data_controller.rb
@@ -7,7 +7,7 @@ class Admin::ExportDataController < Admin::BaseController
     
   def export
     @products  = Product.find_all(params,"xml") 
-    sup_name = Supplier.find(params[:supplier]).name if params[:supplier]      
+    sup_name = params[:supplier].present? ? Supplier.find(params[:supplier]).name : "blank_supplier"
     send_data(XmlDownload.get_xml(@products,params[:options]), :type => :xml, :filename => "export_#{sup_name}_#{Date.today}.xml")
   end
     
