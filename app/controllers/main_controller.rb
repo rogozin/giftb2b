4f067be9d6055f92be62e94cd46763e05cca2fb4
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-09-26 07:30:03 +0400

    ограничение вывода новинок и распродажи до 100 шт на главной сайта + сортировка

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,11 +12,11 @@ class MainController < ApplicationController
   private
 
   def cached_novelty
-    Rails.cache.fetch('novelty_products', :expires_in =>24.hours) { Product.active.novelty.limit(100).all }
+    Rails.cache.fetch('novelty_products', :expires_in =>24.hours) { Product.active.novelty.order('updated_at desc').limit(100).all }
   end
   
   def cached_sale
-    Rails.cache.fetch('sale_products', :expires_in =>24.hours) { Product.active.sale.limit(100).all }
+    Rails.cache.fetch('sale_products', :expires_in =>24.hours) { Product.active.sale.order('updated_at desc').limit(100).all }
   end
 
 
