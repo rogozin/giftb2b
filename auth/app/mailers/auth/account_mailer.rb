67f6f8c9ce17ca6158fcab8b91cf241f889fd391
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-10-13 16:38:44 +0400

    fixed #2090 - тексты

--- auth/app/mailers/auth/account_mailer.rb
+++ auth/app/mailers/auth/account_mailer.rb
@@ -5,26 +5,22 @@ class Auth::AccountMailer < ActionMailer::Base
   
   
   def activation_email(user)
-    @firm = Firm.find(1)
     @user = user
     mail(:to => user.email, :subject => "Активация учетной записи")
   end
   
   def activation_complete(user)
-    @firm = Firm.find(1)
     @user = user
     mail(:to => user.email, :subject => "Активация завершена")
   end
   
   def new_account(user,password)
-    @firm = Firm.find(1)
     @password = password
     @user = user
     mail(:to => user.email, :subject => "Ваша учетная запись на сайте #{default_url_options[:host]}")
   end
 
   def recovery_password(user,password)
-    @firm = Firm.find(1)
     @password = password
     @user = user
     mail(:to => user.email, :subject => "Восстановление пароля на сайте #{default_url_options[:host] }")
