62aa5a34b31d1ec4a05bb114c65fa3d9f05e223e
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-11 17:00:51 +0400

    refs #2397 message send rescue

--- core/app/models/news_observer.rb
+++ core/app/models/news_observer.rb
@@ -1,9 +1,13 @@
 #encoding:utf-8;
 class NewsObserver < ActiveRecord::Observer
   def after_save(news)
-    if news.state_id_changed? && news.state_id_was == 3
+    if news.state_id_changed? && news.state_id_was == 3    
      User.joins(:role_objects).where("active = 1 and (roles.name='Администратор' or roles.name='Редактор контента')").each do |admin|
-      Auth::AdminMailer.new_news_created(news, admin).deliver
+       begin
+        Auth::AdminMailer.new_news_created(news, admin).deliver
+       rescue => err
+        logger.info("message_send_error #{err}")
+       end
      end
    end
   end
