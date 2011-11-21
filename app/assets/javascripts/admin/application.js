ea435521c9d8a28bbc04f84c126e32d4347b86ff
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-21 10:29:22 +0400

    fixed #2555

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -8,16 +8,32 @@
 //= require tinymce-jquery
 //= require admin/tinymce_init
 
-$(function() {
-   $('a.toggle-category').bind('click', function(){
-   $(this).next().toggle();
-   return false;
-  });      
-});
-
 function processing_in_progress() {
   $('#cancel').show(); 
   setInterval(function(){$('#progress').load('/admin/data_changes/get_status');}, 3000);
   return false;
 }
+
+function bindAnimation() {
+  $(".ajax_animation").bind({
+      ajaxStart: function() { $(this).show(); },
+      ajaxStop: function() { $(this).hide(); }
+    });
+}
+
+
+$(function() {
+   $('a.toggle-category').bind('click', function(){
+   $(this).next().toggle();
+   return false;
+  });      
+  bindAnimation();
+  $(".pager a").live("click", function(e) {
+  	$.getScript(this.href, function(){ bindAnimation();});  
+  	return false;
+  });  
+  
+});
+
+
 
