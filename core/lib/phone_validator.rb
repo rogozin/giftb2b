36066aad11c3102374c723a0c619a133f56db225
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-29 16:22:08 +0400

    improve phone validator

--- core/lib/phone_validator.rb
+++ core/lib/phone_validator.rb
@@ -5,8 +5,7 @@ class PhoneValidator < ActiveModel::EachValidator
   @@phone_pattern = /^\+\d{1,3}\(\d{1,5}\)\d{3}-\d{1,4}$/
   
   def validate_each(record, attribute, value)    
-   record.errors[attribute] << (options[:message] || " имеен не правильный формат") unless value =~ @@phone_pattern
-   record.errors[attribute] << (options[:message] || " такой телефон уже есть") if record.is_a?(Client) &&  Firm.exists?(["(id <> :firm_id) and (phone = :val OR phone2 = :val OR phone3 = :val)", {:firm_id => record.persisted? ? record.id : -1, :val => value }])
+   record.errors[attribute] << (options[:message] || " имеет не правильный формат") unless value =~ @@phone_pattern
   end
 
 end
