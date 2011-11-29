601ec986446f90e6e9ade406b3f5270dc0378ca6
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-29 08:09:39 +0400

    add core/lib/settings

---
+++ core/lib/settings.rb
@@ -0,0 +1,20 @@
+module Settings
+  class << self
+    def giftpoisk?
+      false
+    end  
+
+    def giftb2b?
+      true
+    end  
+    
+    def site_name
+      "giftb2b.ru"
+    end    
+    
+    def site_id
+      0
+    end    
+    
+  end
+end
