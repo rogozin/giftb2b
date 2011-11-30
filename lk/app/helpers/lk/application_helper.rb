004adc909492ee02c595930f65c9bd9b70f71805
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-30 16:12:23 +0400

    add group icon )

--- lk/app/helpers/lk/application_helper.rb
+++ lk/app/helpers/lk/application_helper.rb
@@ -8,10 +8,10 @@ module Lk
   
   def render_lk_menu
     res = ""    
-    res << lk_menu_item('Список заказов', orders_path)  if  current_user.is_lk_user?
-  	res << lk_menu_item('Коммерческие предложения', commercial_offers_path) if current_user.is_firm_user?
+    res << lk_menu_item('Поступившие заказы', orders_path)  if  current_user.is_lk_user?
     res << lk_menu_item('Клиенты', firms_path) if  current_user.is_lk_user?
-    res << lk_menu_item('Мой список товаров', products_path) if  current_user.is_lk_user?
+  	res << lk_menu_item('Коммерческие предложения', commercial_offers_path) if current_user.is_firm_user?
+    res << lk_menu_item('Моя база сувениров', products_path) if  current_user.is_lk_user?
     res << lk_menu_item('Мои новости', news_index_path) if  current_user.is_firm_user?
     res << lk_menu_item('Образцы', samples_path)  if current_user.has_role?(:Администратор) || current_user.is_firm_user?
     res << lk_menu_item('Пользователи', accounts_path) if  current_user.is_firm_manager?
