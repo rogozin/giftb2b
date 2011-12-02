2628a91cd1aec543294b5d5af29bb9cfde6327e0
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-12-02 16:05:14 +0400

    fixed #2720 - изменения на странице КП

--- lk/app/helpers/lk/commercial_offers_helper.rb
+++ lk/app/helpers/lk/commercial_offers_helper.rb
@@ -1,6 +1,10 @@
 module Lk::CommercialOffersHelper
   
   def price_with_sale price, sale
-    price - price*(sale.to_f/100)
+    price - sale_size(price, sale)
+  end
+  
+  def sale_size price, sale
+    price*(sale.to_f/100)
   end
 end
---
+++ lk/app/helpers/lk/products_helper.rb
@@ -0,0 +1,12 @@
+module Lk::ProductsHelper
+  
+  def article lk_product
+    lk_product.is_my? ? lk_product.article  : content_tag(:span, lk_product.product.unique_code, :class => "art-product") + content_tag(:span, " (#{lk_product.product.article})", :class => "art-sup")
+  end
+  
+  
+  def product_image_link lk_product
+    link_to_if lk_product.product, image_tag(lk_product && lk_product.picture ? lk_product.picture.url(:thumb) : "default_image.jpg"), main_app.product_path(lk_product.product), :target => "_blank"
+  end
+  
+end
