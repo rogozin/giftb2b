49ebfede61ceb50c502e90331ccb1941ab4fde27
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-12-15 13:47:36 +0400

    comm offer excel output with sale

--- lk/lib/excel.rb
+++ lk/lib/excel.rb
@@ -19,7 +19,7 @@ module Gift
          bold = workbook.add_format(:bold => 1)
          
          worksheet.write(7,2,"Артикул", bold) unless hide_article
-         worksheet.write_row(7,hide_article ? 2 : 3,["Название товара","Цена","Кол-во","Сумма","Материал","Цвет","Размер", "Упаковка", "Нанесение", "Склад"], bold)    
+         worksheet.write_row(7,hide_article ? 2 : 3,["Название товара","Цена","Кол-во","Скидка", "Сумма со скидкой","Материал","Цвет","Размер", "Упаковка", "Нанесение", "Склад"], bold)    
          worksheet.write(7, hide_article ? 12 : 13, "Описание", bold) unless hide_description
          @index = 0
          commercial_offer.commercial_offer_items.each_with_index do |i, index|
@@ -27,7 +27,8 @@ module Gift
            worksheet.set_row(row_target, 100)
            worksheet.insert_image(row_target, 0, i.lk_product.picture.path(:thumb))
            worksheet.write(row_target, 2, i.lk_product.article, format1) unless hide_article 
-           worksheet.write_row(row_target, hide_article ? 2 : 3, [i.lk_product.short_name, i.lk_product.price, i.quantity, i.quantity * i.lk_product.price, i.lk_product.factur, i.lk_product.color, i.lk_product.size, i.lk_product.box, i.lk_product.infliction, i.lk_product.store_count],format1)
+           price_with_sale = i.lk_product.price * (1-i.sale.to_f/100)
+           worksheet.write_row(row_target, hide_article ? 2 : 3, [i.lk_product.short_name, i.lk_product.price, i.quantity, i.sale, i.quantity * price_with_sale, i.lk_product.factur, i.lk_product.color, i.lk_product.size, i.lk_product.box, i.lk_product.infliction, i.lk_product.store_count],format1)
            worksheet.write(row_target, hide_article ? 12 : 13, i.lk_product.description, format1) unless hide_description
            @index = index
          end
