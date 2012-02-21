5c18533a58b16963d349279d89673151fe4bc087
Ilya Rogozin <ilya.rogozin@gmail.com>
2012-02-21 22:11:06 +0400

    return fresh_when

--- base/app/controllers/products_controller.rb
+++ base/app/controllers/products_controller.rb
@@ -4,8 +4,8 @@ class ProductsController < BaseController
   def show
       @product = Product.find_by_permalink(params[:id])
       return not_found unless @product
-#      expires_in 1.minutes, :public => true unless current_user      
-#     fresh_when(:etag => @product, :last_modified => @product.updated_at, :public => true) unless current_user     
+      current_user ? expires_now :  expires_in(1.minutes, :public => true)
+      fresh_when(:etag => @product, :last_modified => @product.updated_at, :public => true) unless current_user     
   end
 
 end
