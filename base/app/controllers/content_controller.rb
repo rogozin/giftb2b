49b29318ff3894b3c4c338cb1866674a0e721815
Ilya Rogozin <ilya.rogozin@gmail.com>
2012-02-21 21:51:48 +0400

    remove fresh_when tags

--- base/app/controllers/content_controller.rb
+++ base/app/controllers/content_controller.rb
@@ -3,7 +3,7 @@ class ContentController < ApplicationController
 
   def show
     @content = Content.active.find_by_permalink(params[:id])
-    fresh_when(:etag => @content, :last_modified => @content.updated_at, :public => true) unless current_user           
+#    fresh_when(:etag => @content, :last_modified => @content.updated_at, :public => true) unless current_user           
   end
 
 end
--- base/app/controllers/products_controller.rb
+++ base/app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < BaseController
       @product = Product.find_by_permalink(params[:id])
       return not_found unless @product
 #      expires_in 1.minutes, :public => true unless current_user      
-      fresh_when(:etag => @product, :last_modified => @product.updated_at, :public => true) unless current_user     
+#     fresh_when(:etag => @product, :last_modified => @product.updated_at, :public => true) unless current_user     
   end
 
 end
