3d8ea98f5088035566cda751d069b2425a8b530a
Ilya Rogozin <ilya.rogozin@gmail.com>
2012-02-22 13:22:17 +0400

    devise

--- auth/app/controllers/auth/application_controller.rb
+++ auth/app/controllers/auth/application_controller.rb
@@ -1,5 +1,17 @@
 module Auth
   class ApplicationController < ActionController::Base
+    
+    
+   private
+   
+   def after_sign_out_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || main_app.root_path
+   end 
+
+   def after_sign_in_path_for(resource_or_scope)
+      stored_location_for(resource_or_scope) || main_app.root_path
+   end
+    
   end
   
 end
--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -18,10 +18,10 @@ Auth::Engine.routes.draw do
 #  match 'user_session' => "user_session#create", :as => :create_session
 #  match 'login/:token' => "users#login_by_token", :as => :login_by_token
 #  #resource :user_session
-#  match 'profile' => "profile#edit", :as => :profile
-#  match 'change_profile' => 'profile#update', :as => :change_profile
-#  post 'who_are_you' => "users#who_are_you", :as => :who_are_you
-#  resources :users, :only => [:create, :edit, :update] do 
-#    post :change_password, :on => :collection
-#  end
+  match 'profile' => "profile#edit", :as => :profile
+  match 'change_profile' => 'profile#update', :as => :change_profile
+  post 'who_are_you' => "users#who_are_you", :as => :who_are_you
+  resources :users, :only => [:create, :edit, :update] do 
+    post :change_password, :on => :collection
+  end
 end
